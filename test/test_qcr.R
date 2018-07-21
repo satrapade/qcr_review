5a061691479105f76d38607dc110d3f150a15243
nassuphis <nicknassuphis@gmail.com>
2018-07-21 04:54:22 +0100

    read qcr dataset

--- test/test_qcr.R
+++ test/test_qcr.R
@@ -1,11 +1,13 @@
 
 
 qcr_tr<-fread("currency_total_returns.csv") %>% 
-{as.matrix(.[,2:(ncol(.)-1)],rownames=.$rn)}
+{as.matrix(.[,2:(ncol(.)-1)],rownames=.$V1)}
 
 
 qcr_msig<-fread("momentum_signal.csv") %>% 
-{as.matrix(.[,2:(ncol(.)-1)],rownames=stri_sub(.$V1,10))}
+{as.matrix(.[,2:(ncol(.)-1)],rownames=stri_sub(.$V1,1,10))}
+
+x<-head(qcr_msig[,colnames(qcr_tr)],-2)*tail(qcr_tr,-1)
 
 
 
