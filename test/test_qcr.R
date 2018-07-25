742fd4b33e260776f6505647f655a7ab816a66a3
nassuphis <nicknassuphis@gmail.com>
2018-07-25 22:25:39 +0100

    added correl trajectories

--- test/test_qcr.R
+++ test/test_qcr.R
@@ -1,7 +1,29 @@
 
 
-qcr_sv<-(head(apply(qcr_stvf,2,roll_mean,n=30,fill=0,align="r"),-1)*tail(qcr_tr,-1)) %>% 
-{setNames(.,tail(rownames(qcr_tr),-1))} 
+x1<-apply(qcr_tr,2,roll_sumr,n=30,fill=0)
+y1<-apply(qcr_tr,2,roll_sumr,n=120,fill=0)
 
-plot(cumsum(rowSums(qcr_sv[-(1:ss),])),type="l")
+x2<-apply(qcr_tr^2,2,roll_meanr,n=90,fill=0)
+y2<-apply(qcr_tr^2,2,roll_meanr,n=250,fill=0)
 
+z<-apply(qcr_tr,2,cumsum)
+
+scor<-function(i=120,n=3000){
+  res<-sign(tail(z,-i)-head(z,-i))
+  data.table(
+    pm=mean(tail(sign(tail(qcr_msig,-i))*res,n)),
+    cm=mean(tail(sign(tail(qcr_carry_momentum,-i))*res,n)),
+    cl=mean(tail(sign(tail(qcr_carry_level,-i))*res,n)),
+    lv=mean(tail(sign(tail(qcr_ltvf,-i))*res,n)),
+    sv=mean(tail(sign(tail(qcr_stvf,-i))*res,n))
+  )
+}
+
+res<-seq(10,500,length.out=100) %>% as.integer %>% {data.table(
+  i=.,
+  do.call(rbind,mapply(scor,.,SIMPLIFY=FALSE))
+)}
+
+res %>%
+{ melt(.,id.vars="i",measure.vars=names(.)[-1]) } %>%
+ ggplot()+geom_point(aes(x=i,y=value,col=variable,group=variable))
