c8a33255fc221ac3e63072d2ef01bd0b30b2836f
nassuphis <nicknassuphis@gmail.com>
2018-07-25 08:10:35 +0100

    stuff

--- test/test_qcr.R
+++ test/test_qcr.R
@@ -1,8 +1,6 @@
 
-roll()
 
-
-qcr_sv<-(head(apply(qcr_stvf,2,roll_mean,n=5,fill=0,align="r"),-1)*tail(qcr_tr,-1)) %>% 
+qcr_sv<-(head(apply(qcr_stvf,2,roll_mean,n=30,fill=0,align="r"),-1)*tail(qcr_tr,-1)) %>% 
 {setNames(.,tail(rownames(qcr_tr),-1))} 
 
 plot(cumsum(rowSums(qcr_sv[-(1:ss),])),type="l")
