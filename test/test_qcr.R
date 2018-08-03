3ef5542abbf90e06ae62b975f9db903b02d59cfc
nassuphis <nicknassuphis@gmail.com>
2018-08-03 12:42:34 +0300

    added stuff

--- test/test_qcr.R
+++ test/test_qcr.R
@@ -1,29 +1,33 @@
 
+require(stringi)
+require(scales)
+require(digest)
+require(clue)
+require(FRAPO)
+require(data.table)
+require(Matrix)
+require(Matrix.utils)
+require(magrittr)
+require(gsubfn)
+require(Hmisc)
+require(ggplot2)
+require(magick)
+require(DBI)
+require(readxl)
+require(Rtsne)
+require(knitr)
+require(stringi)
+require(RcppRoll)
+require(magick)
+require(corrplot)
 
-x1<-apply(qcr_tr,2,roll_sumr,n=30,fill=0)
-y1<-apply(qcr_tr,2,roll_sumr,n=120,fill=0)
 
-x2<-apply(qcr_tr^2,2,roll_meanr,n=90,fill=0)
-y2<-apply(qcr_tr^2,2,roll_meanr,n=250,fill=0)
+x<-fread("EURUSD.csv") %>% 
+  {do.call(cbind,.[,-1])}
 
-z<-apply(qcr_tr,2,cumsum)
+x %>% 
+  ggplot() +
+  geom_line(aes(x=as.Date(date),y=value,col=variable))
 
-scor<-function(i=120,n=3000){
-  res<-sign(tail(z,-i)-head(z,-i))
-  data.table(
-    pm=mean(tail(sign(tail(qcr_msig,-i))*res,n)),
-    cm=mean(tail(sign(tail(qcr_carry_momentum,-i))*res,n)),
-    cl=mean(tail(sign(tail(qcr_carry_level,-i))*res,n)),
-    lv=mean(tail(sign(tail(qcr_ltvf,-i))*res,n)),
-    sv=mean(tail(sign(tail(qcr_stvf,-i))*res,n))
-  )
-}
 
-res<-seq(10,500,length.out=100) %>% as.integer %>% {data.table(
-  i=.,
-  do.call(rbind,mapply(scor,.,SIMPLIFY=FALSE))
-)}
 
-res %>%
-{ melt(.,id.vars="i",measure.vars=names(.)[-1]) } %>%
- ggplot()+geom_point(aes(x=i,y=value,col=variable,group=variable))
